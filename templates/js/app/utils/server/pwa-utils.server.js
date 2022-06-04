b13f99d46db5f7206957efbdc09187a0e0f2c2c8
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-06-04 22:49:03 +0100

    Server template desc. via jsdoc

--- templates/js/app/utils/server/pwa-utils.server.js
+++ templates/js/app/utils/server/pwa-utils.server.js
@@ -6,6 +6,12 @@ export async function SaveSubscription(sub) {
   await storage.setItem("subscription", sub);
 }
 
+/**
+ * Pushes and triggers a notification to the client-side of your app straight from the server.
+ * 
+ * @param {string} content - An object consisting of the Notification's info to be sent over the server.
+ * @param {number} delay - The delay in milliseconds before the text is copied (defaults to 0)
+ */
 export async function PushNotification(content, delay = 0) {
   if (!process.env.VAPID_PUBLIC_KEY || !process.env.VAPID_PRIVATE_KEY) {
     console.log(
