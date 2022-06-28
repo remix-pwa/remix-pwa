9eadb71b9639f07cc5d32474b08312519b2402d9
Thomas Markiewicz <tomek@librem.one>
2022-06-27 19:58:30 -0500

    Fix cloning of postMessage object; matches could include functions

--- templates/ts/app/root.ts
+++ templates/ts/app/root.ts
@@ -7,23 +7,23 @@ React.useEffect(() => {
   isMount = false;
   if ("serviceWorker" in navigator) {
     if (navigator.serviceWorker.controller) {
-      navigator.serviceWorker.controller?.postMessage({
+      navigator.serviceWorker.controller?.postMessage(JSON.stringify({
         type: "REMIX_NAVIGATION",
         isMount: mounted,
         location,
         matches,
         manifest: window.__remixManifest,
-      });
+      }));
     } else {
       let listener = async () => {
         await navigator.serviceWorker.ready;
-        navigator.serviceWorker.controller?.postMessage({
+        navigator.serviceWorker.controller?.postMessage(JSON.stringify({
           type: "REMIX_NAVIGATION",
           isMount: mounted,
           location,
           matches,
           manifest: window.__remixManifest,
-        });
+        }));
       };
       navigator.serviceWorker.addEventListener("controllerchange", listener);
       return () => {
