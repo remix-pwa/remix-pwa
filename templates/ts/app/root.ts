32bbd7551396f2115a53b85235911af9879f1edb
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-07-02 21:09:09 +0100

    Updated `root` for better usages

--- templates/ts/app/root.ts
+++ templates/ts/app/root.ts
@@ -1,29 +1,28 @@
 let location = useLocation();
 let matches = useMatches();
 
-let isMount = true;
 React.useEffect(() => {
   let mounted = isMount;
   isMount = false;
   if ("serviceWorker" in navigator) {
     if (navigator.serviceWorker.controller) {
-      navigator.serviceWorker.controller?.postMessage({
+      navigator.serviceWorker.controller?.postMessage(JSON.stringify({
         type: "REMIX_NAVIGATION",
         isMount: mounted,
         location,
         matches,
         manifest: window.__remixManifest,
-      });
+      }));
     } else {
       let listener = async () => {
         await navigator.serviceWorker.ready;
-        navigator.serviceWorker.controller?.postMessage({
+        navigator.serviceWorker.controller?.postMessage(JSON.stringify({
           type: "REMIX_NAVIGATION",
           isMount: mounted,
           location,
           matches,
           manifest: window.__remixManifest,
-        });
+        }));
       };
       navigator.serviceWorker.addEventListener("controllerchange", listener);
       return () => {
