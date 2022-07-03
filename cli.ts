a6b4740ec08478c6e4292eac26ed104c6e64cddb
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-07-03 23:33:34 +0100

    Another test...

--- cli.ts
+++ cli.ts
@@ -92,14 +92,15 @@ async function Run(projectDir: string, lang: "ts" | "js", dir: string, cache: st
   rootArray.unshift("import React from 'react';");
   const RootDirContent3 = rootArray.join("\n");
 
-  const RootDirContent4 =
-    RootDirContent3.slice(0, totalImportCount) +
-    "\n" +
-    localeRootDir +
-    "\n" +
-    RootDirContent3.slice(totalImportCount + 1);
+  console.log(RootDirContent3)
+  // const RootDirContent4 =
+  //   RootDirContent3.slice(0, totalImportCount) +
+  //   "\n" +
+  //   localeRootDir +
+  //   "\n" +
+  //   RootDirContent3.slice(totalImportCount + 1);
 
-  const NewContent = RootDirContent4.includes(localeRootDir)
+  const NewContent = RootDirContent3.includes(localeRootDir)
     ? RootDirContent3
     : RootDirNull.slice(0, index - 1) + "\n" + localeRootDir + "\n" + RootDirNull.slice(index);
   const formatted: string = prettier.format(NewContent, { parser: `babel${parser}` });
