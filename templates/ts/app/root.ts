ebd9a5c010c3c6e7fca8a0b5c7afe4f12f308929
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-07-05 13:11:58 +0100

    Removed serialization of SW messages

--- templates/ts/app/root.ts
+++ templates/ts/app/root.ts
@@ -6,23 +6,23 @@ React.useEffect(() => {
   isMount = false;
   if ("serviceWorker" in navigator) {
     if (navigator.serviceWorker.controller) {
-      navigator.serviceWorker.controller?.postMessage(JSON.stringify({
+      navigator.serviceWorker.controller?.postMessage({
         type: "REMIX_NAVIGATION",
         isMount: mounted,
         location,
         matches,
         manifest: window.__remixManifest,
-      }));
+      });
     } else {
       let listener = async () => {
         await navigator.serviceWorker.ready;
-        navigator.serviceWorker.controller?.postMessage(JSON.stringify({
+        navigator.serviceWorker.controller?.postMessage({
           type: "REMIX_NAVIGATION",
           isMount: mounted,
           location,
           matches,
           manifest: window.__remixManifest,
-        }));
+        });
       };
       navigator.serviceWorker.addEventListener("controllerchange", listener);
       return () => {
