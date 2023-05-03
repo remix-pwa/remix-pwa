0473d453f884630856b33666771a46c3a7c7c241
ShafSpecs <abbadshaf05@gmail.com>
2023-05-03 15:10:09 +0100

    chore: new service worker templates

---
+++ templates/js/app/precache.worker.js
@@ -0,0 +1,54 @@
+/// <reference lib="WebWorker" />
+
+import { CacheFirst, NetworkFirst, PrecacheHandler, matchRequest } from "@remix-pwa/sw";
+
+let self;
+
+const PAGES = "page-cache";
+const DATA = "data-cache";
+const ASSETS = "assets-cache";
+const staticAssets = ["/build/", "/icons/"];
+
+const assetHandler = new CacheFirst({ cacheName: ASSETS });
+const pageHandler = new NetworkFirst({ cacheName: PAGES });
+const dataHandler = new NetworkFirst({ cacheName: DATA, isLoader: true });
+
+const precacheHandler = new PrecacheHandler();
+
+const fetchHandler = async (event) => {
+  const { request } = event;
+  const match = matchRequest(request, staticAssets);
+
+  switch (match) {
+    case "asset":
+      return assetHandler.handle(request);
+    case "document":
+      return pageHandler.handle(request);
+    case "loader":
+      return dataHandler.handle(request);
+    default:
+      return fetch(request.clone());
+  }
+};
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("message", (event) => {
+  event.waitUntil(precacheHandler.handle(event, {
+    caches: {
+      DOCUMENT_CACHE: PAGES,
+      DATA_CACHE: DATA,
+      ASSET_CACHE: ASSETS,
+    }
+  }));
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetchHandler(event));
+});
---
+++ templates/ts/app/entry.worker.ts
@@ -0,0 +1,69 @@
+/// <reference lib="WebWorker" />
+
+import {
+  CacheFirst,
+  NetworkFirst,
+  RemixNavigationHandler,
+  isAssetRequest,
+  isDocumentRequest,
+  isLoaderRequest,
+  logger,
+} from "@remix-pwa/sw";
+
+export type {};
+declare let self: ServiceWorkerGlobalScope;
+
+const ASSETS = "asset-cache";
+const DOCUMENTS = "page-cache";
+const DATA = "data-cache";
+
+const loaderHandler = new NetworkFirst({
+  cacheName: DATA,
+  isLoader: true,
+});
+
+const documentHandler = new NetworkFirst({
+  cacheName: DOCUMENTS,
+});
+
+const assetHandler = new CacheFirst({
+  cacheName: ASSETS,
+});
+
+const fetchHandler = (event: FetchEvent): Promise<Response> => {
+  if (isDocumentRequest(event.request)) {
+    return documentHandler.handle(event.request);
+  } else if (isLoaderRequest(event.request)) {
+    return loaderHandler.handle(event.request);
+  } else if (isAssetRequest(event.request)) {
+    return assetHandler.handle(event.request);
+  }
+
+  return fetch(event.request.clone());
+};
+
+const messageHandler = new RemixNavigationHandler();
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetchHandler(event));
+});
+
+self.addEventListener("message", (event) => {
+  logger.log("message", event.data.isMount);
+  event.waitUntil(
+    messageHandler.handle(event, {
+      caches: {
+        DATA,
+        PAGES: DOCUMENTS,
+      },
+    })
+  );
+});
