97f93ad7d506f77f6fd4c8b294916e7f79ba634a
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2023-06-11 22:20:54 +0100

    workers: updated workers template to make it more minimal

--- templates/js/app/entry.worker.js
+++ templates/js/app/entry.worker.js
@@ -1,45 +1,13 @@
 /// <reference lib="WebWorker" />
 
-import {
-  CacheFirst,
-  NetworkFirst,
-  RemixNavigationHandler,
-  isAssetRequest,
-  isDocumentRequest,
-  isLoaderRequest,
-  logger,
-} from "@remix-pwa/sw";
+import { RemixNavigationHandler } from "@remix-pwa/sw";
 
 let self;
 
 const ASSETS = "asset-cache";
 const DOCUMENTS = "page-cache";
 const DATA = "data-cache";
-
-const loaderHandler = new NetworkFirst({
-  cacheName: DATA,
-  isLoader: true,
-});
-
-const documentHandler = new NetworkFirst({
-  cacheName: DOCUMENTS,
-});
-
-const assetHandler = new CacheFirst({
-  cacheName: ASSETS,
-});
-
-const fetchHandler = (event) => {
-  if (isDocumentRequest(event.request)) {
-    return documentHandler.handle(event.request);
-  } else if (isLoaderRequest(event.request)) {
-    return loaderHandler.handle(event.request);
-  } else if (isAssetRequest(event.request)) {
-    return assetHandler.handle(event.request);
-  }
-
-  return fetch(event.request.clone());
-};
+const STATIC_ASSETS = ["/build/", "/icons/", "/favicon.ico"];
 
 const messageHandler = new RemixNavigationHandler({
   dataCacheName: DATA,
@@ -55,10 +23,9 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetchHandler(event));
+  event.respondWith(fetch(event.request.clone()));
 });
 
 self.addEventListener("message", (event) => {
-  logger.log("message", event.data.isMount);
   event.waitUntil(messageHandler.handle(event));
 });
--- templates/ts/app/entry.worker.ts
+++ templates/ts/app/entry.worker.ts
@@ -1,14 +1,6 @@
 /// <reference lib="WebWorker" />
 
-import {
-  CacheFirst,
-  NetworkFirst,
-  RemixNavigationHandler,
-  isAssetRequest,
-  isDocumentRequest,
-  isLoaderRequest,
-  logger,
-} from "@remix-pwa/sw";
+import { RemixNavigationHandler } from "@remix-pwa/sw";
 
 export type {};
 declare let self: ServiceWorkerGlobalScope;
@@ -16,31 +8,7 @@ declare let self: ServiceWorkerGlobalScope;
 const ASSETS = "asset-cache";
 const DOCUMENTS = "page-cache";
 const DATA = "data-cache";
-
-const loaderHandler = new NetworkFirst({
-  cacheName: DATA,
-  isLoader: true,
-});
-
-const documentHandler = new NetworkFirst({
-  cacheName: DOCUMENTS,
-});
-
-const assetHandler = new CacheFirst({
-  cacheName: ASSETS,
-});
-
-const fetchHandler = (event: FetchEvent): Promise<Response> => {
-  if (isDocumentRequest(event.request)) {
-    return documentHandler.handle(event.request);
-  } else if (isLoaderRequest(event.request)) {
-    return loaderHandler.handle(event.request);
-  } else if (isAssetRequest(event.request)) {
-    return assetHandler.handle(event.request);
-  }
-
-  return fetch(event.request.clone());
-};
+const STATIC_ASSETS = ["/build/", "/icons/", "/favicon.ico"];
 
 const messageHandler = new RemixNavigationHandler({
   dataCacheName: DATA,
@@ -56,10 +24,9 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetchHandler(event));
+  event.respondWith(fetch(event.request.clone()));
 });
 
 self.addEventListener("message", (event) => {
-  logger.log("message", event.data.isMount);
   event.waitUntil(messageHandler.handle(event));
 });
