c477b8ec0b0ab7e2dc56ab07079e848049f543d0
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2023-06-12 12:54:56 +0100

    updated help message

--- cli.ts
+++ cli.ts
@@ -272,24 +272,24 @@ Usage:  npx remix-pwa@latest [OPTIONS]
 A stand-alone package for integrating PWA solutions into Remix application.
   
 ${colorette.underline(colorette.whiteBright("Options:"))}
---typescript, --ts        Create project with typescript template
---no-typescript, --no-ts  Create project with javascript template
---workbox                 Integrate workbox into your project
---no-workbox              Skip integrating workbox into your project  
---install                 Install dependencies after creating the project
---no-install              Skip the installation process
---package-manager, --pm   Preferred package manager if your project is not using any
---cache                   Preferred \`Caching Strategy\` for the service worker. Either \`jit\` or \`pre\`
---features, --feat        \`Remix-Pwa\` features you want to include:
-                          - 'sw' for Service Workers
-                          - 'manifest' for Web Manifest
-                          - 'push' for Push Notifications
-                          - 'utils' for PWA Client Utilities
-                          - 'icons' for Development Icons
---dir                     The location of your Remix \`app\` directory
---help, -h                Print this help message and exit
---version, -v             Print the CLI version and exit
---docs                    Get the link to the remix-pwa docs`;
+--typescript, --ts              Create project with typescript template
+--no-typescript, --no-ts, --js  Create project with javascript template
+--workbox                       Integrate workbox into your project
+--no-workbox                    Skip integrating workbox into your project  
+--install                       Install dependencies after creating the project
+--no-install                    Skip the installation process
+--package-manager, --pm         Preferred package manager if your project is not using any
+--cache                         Preferred \`Caching Strategy\` for the service worker. Either \`jit\` or \`pre\`
+--features, --feat              \`Remix-Pwa\` features you want to include:
+                                - 'sw' for Service Workers
+                                - 'manifest' for Web Manifest
+                                - 'push' for Push Notifications
+                                - 'utils' for PWA Client Utilities
+                                - 'icons' for Development Icons
+--dir                           The location of your Remix \`app\` directory
+--help, -h                      Print this help message and exit
+--version, -v                   Print the CLI version and exit
+--docs                          Print the link to the remix-pwa docs and exit`;
 
 const getCliVersion = () => {
   const version = require("../package.json").version;
@@ -325,6 +325,7 @@ async function cli() {
     "--ts": "--typescript",
     "--feat": "--features",
     "--no-ts": "--no-typescript",
+    "--js": "--no-typescript",
     "--pm": "--package-manager",
   });
 
